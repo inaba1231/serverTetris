1e13a22ced92945d24765ebaa05c877d68c3bd87
Inaba Kazuhiro <inaba.k94@gmail.com>
2017-04-08 12:45:35 +0800

    Exchange every 40 generation

--- src/main/java/Tetris/PlayerSkeleton.java
+++ src/main/java/Tetris/PlayerSkeleton.java
@@ -44,7 +44,7 @@ public class PlayerSkeleton {
 
     public static void main(String[] args) {
         IO io = new IO();
-        Ecosystem ecosystem = new Ecosystem(io.importEcosystem(), 1, 40);
+        Ecosystem ecosystem = new Ecosystem(io.importEcosystem(), 201, 40);
         while(true) {
             ecosystem.play();
         }
